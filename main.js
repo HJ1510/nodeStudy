b1db9ba8a4e053416ec9b974f291db45d1588fa1
HJ1510 <yhj221004@gmail.com>
2023-03-14 11:19:55 +0900

    node basic #1 코어모듈

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let m = require("./math-tools");
+// const fs = require("fs");
 
-console.log(m.PI);
-console.log(m.add(1, 2));
-console.log(m.subtract(1, 2));
-console.log(m.multiply(1, 2));
-console.log(m.divide(1, 2));
+// let filelist = fs.readdirSync("."); // 디렉토리 안의 항목을 배열로 리턴
+// console.log(filelist);
+
+// fs.writeFileSync("new", "Hello!!!!"); // 'new'파는 파일에 두 번째 파라미터의 내용을 적음 'new' 없으면 파일 생성 기존에 있는 파일이면 덮어쓰기
+// fs.unlinkSync("./new"); // 'new' 파일 삭제
+
+const os = require("os");
+
+console.log(os.cpus()); // 컴퓨터 정보 출력

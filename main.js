526c83e0e7ccda4fff567b69a34cb418e72435ef
HJ1510 <yhj221004@gmail.com>
2023-03-14 14:59:08 +0900

    node basic #1 비동기 실행, EventEmitter(1)

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-const cowsay = require("cowsay");
+// const fs = require("fs");
 
-console.log(
-  cowsay.say({
-    text: "HI!!",
-  })
-);
+// // console.log("Start");
+
+// // let content = fs.readFileSync("./new", "utf8");
+// // console.log(content);
+
+// // console.log("Finish");
+
+// console.log("Start");
+
+// fs.readFile("./new", "utf8", (error, data) => {
+//   console.log(data);
+// });
+
+// console.log("Finish");
+
+// const EventEmitter = require("events");
+
+// const myEmitter = new EventEmitter();
+
+// const cbArr = [];
+
+// cbArr[0] = () => {
+//   console.log("A");
+// };
+
+// cbArr[1] = () => {
+//   console.log("B");
+// };
+
+// myEmitter.on("test", cbArr[0]);
+// myEmitter.on("test", cbArr[1]);
+
+// myEmitter.off("test", cbArr[0]);
+// myEmitter.off("test", cbArr[1]);
+
+// myEmitter.emit("test");

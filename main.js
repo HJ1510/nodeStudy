a07574a67cd40cb088f3a2467a7248252c377293
HJ1510 <yhj221004@gmail.com>
2023-03-14 19:55:57 +0900

    코드 정리

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-let url = new URL(
-  "https://www.google.com/webhp?hl=ko&sa=X&ved=0ahUKEwjIlczz9tr9AhUHpVYBHZ8vBrYQPAgI"
-);
+const http = require('http');
 
-console.log(url.protocol);
-console.log(url.host);
-console.log(url.pathname);
-console.log(url.search);
+const members = ['dooly', 'ddochi', 'heedong', 'gildong'];
+
+const server = http.createServer((request, response) => {
+  if (request.url === '/') {
+    response.end('<h1>welcome</h1>');
+  } else if (request.url === '/users') {
+    response.end(`<h1>${members}</h1>`);
+  } else if (request.url.split('/')[1] === 'users') {
+    const membersIdx = request.url.split('/')[2];
+    const membersName = members[membersIdx - 1];
+    response.end(`<h1>${membersName}</h1>`);
+  } else {
+    response.end('<h1>Service unavailable</h1>');
+  }
+});
+
+server.listen(5000);

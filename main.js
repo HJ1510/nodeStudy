79ec944400e1fc207ada316cc20d36dd2a030a9a
HJ1510 <yhj221004@gmail.com>
2023-03-15 09:47:37 +0900

    node basic #2 express모듈 basic

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 const http = require('http');
 
+const express = require('express');
+
+const app = express();
+
 const members = ['dooly', 'ddochi', 'heedong', 'gildong'];
 
-const server = http.createServer((request, response) => {
-  if (request.url === '/') {
-    response.end('<h1>welcome</h1>');
-  } else if (request.url === '/users') {
-    response.end(`<h1>${members}</h1>`);
-  } else if (request.url.split('/')[1] === 'users') {
-    const membersIdx = request.url.split('/')[2];
-    const membersName = members[membersIdx - 1];
-    response.end(`<h1>${membersName}</h1>`);
-  } else {
-    response.end('<h1>Service unavailable</h1>');
-  }
+app.get('/', (request, response) => {
+  response.end('<h1>welcome</h1>');
 });
 
-server.listen(5000);
+app.get('/members', (request, response) => {
+  response.end(`<h1>${members}</h1>`);
+});
+
+app.get('/members/:id', (request, response) => {
+  // console.log(request.params);
+  const membersName = members[request.params.id - 1];
+  response.end(`<h1>${membersName}</h1>`);
+});
+
+app.get('*', (request, response) => {
+  response.end('<h1>Service unavailable</h1>');
+});
+
+app.listen(5000);
+
+// const server = http.createServer((request, response) => {
+//   if (request.url === '/') {
+//     response.end('<h1>welcome</h1>');
+//   } else if (request.url === '/members') {
+//     response.end(`<h1>${members}</h1>`);
+//   } else if (request.url.split('/')[1] === 'users') {
+//     const membersIdx = request.url.split('/')[2];
+//     const membersName = members[membersIdx - 1];
+//     response.end(`<h1>${membersName}</h1>`);
+//   } else {
+//     response.end('<h1>Service unavailable</h1>');
+//   }
+// });
+
+// server.listen(5000);

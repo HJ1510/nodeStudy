962711f7031d700c032110cfc615e42fbb1fef92
HJ1510 <yhj221004@gmail.com>
2023-03-16 11:23:02 +0900

    mongoDB 회원가입 password 암호화

--- expressPractice/boiler/index.js
+++ expressPractice/boiler/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 5000;
+const { User } = require('./models/User');
 
 const config = require('./config/keys');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose
@@ -12,6 +20,23 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!!');
+});
+
+app.post('/register', (req, res) => {
+  // 회원가입시 필요한 정보를 클라이언트에서 가져오고
+  // 그 정보를 데이터베이스에 삽입
+  const user = new User(req.body);
+  // user모델에 정보가 저장되고 실패시 에러메세지 출력
+  user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
 });
 
 app.listen(port, () => {
--- expressPractice/boiler/models/User.js
+++ expressPractice/boiler/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // 10자리의 salt를 생성
 
 const userSchema = mongoose.Schema(
   {
@@ -35,6 +37,30 @@ const userSchema = mongoose.Schema(
   },
   { timestamp: true }
 );
+
+// pre() mongoose에서 지원 user.save() 전에 처리할 함수 지정
+userSchema.pre('save', function (next) {
+  var user = this;
+  if (user.isModified('password')) {
+    // isModified() mongoose 지원 파라미터 값이 바뀔 때 true 반환
+    // Salt 이용 비밀번호 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      // genSalt : Salt 생성
+
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        // user.password: 사용자가 실제 입력한 비밀번호
+        // hash: 암호화된 비밀번호
+        // Store hash in your password DB.
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 const User = mongoose.model('User', userSchema);
 
